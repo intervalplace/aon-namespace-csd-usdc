5009f5ea4bceb4ec830f7667984a5c660b1ae626
intervalplace <julnerapple@hotmail.com>
2026-07-01 23:34:12 +0200

    Add files via upload

--- src/validators/receipt.ts
+++ src/validators/receipt.ts
@@ -1,20 +1,5 @@
 import type { AonObject } from "@intervalplace/aon-sdk";
-import {
-  requireObject,
-  type ValidationGraph,
-} from "./graph.js";
 
 export async function validateReceipt(_obj: AonObject) {
   return;
-}
-
-export async function validateReceiptGraph(
-  obj: AonObject,
-  graph: ValidationGraph
-) {
-  await validateReceipt(obj);
-
-  for (const ref of obj.references ?? []) {
-    requireObject(graph, ref);
-  }
 }
