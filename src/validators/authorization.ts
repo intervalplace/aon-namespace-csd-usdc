0426feac2ea66ad596ba0b4aca1715324b77843f
intervalplace <julnerapple@hotmail.com>
2026-07-04 00:07:26 +0200

    Delete src/validators/authorization.ts

--- src/validators/authorization.ts
+++
@@ -1,6 +0,0 @@
-import type { AonObject } from "@intervalplace/aon-sdk";
-
-export async function validateAuthorization(obj: AonObject) {
-  if (obj.objectType !== "authorization") return;
-  if (!obj.payload?.authorization) throw new Error("AUTH_PAYLOAD_MISSING");
-}
